1053da3814451a60e7d0c18cc16ba1d7594c7945
Момчил Миланов <deadman_kula@abv.bg>
2021-01-30 18:12:45 +0200

    Add project files.

---
+++ ProjectsCreation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Вход
+            //           От конзолата се четат 2 реда:
+            //⦁	Името на архитекта - текст
+            string architect = Console.ReadLine();
+            //⦁	Брой на проектите - цяло число в интервала[0… 100]
+            int projects = int.Parse(Console.ReadLine());
+            int neededHours = projects * 3;
+            //Изход
+            //На конзолата се отпечатва:
+            //⦁	"The architect {името на архитекта} will need {необходими часове} hours to complete {брой на проектите} project/s."
+            Console.WriteLine($"The architect {architect} will need {neededHours} hours to complete {projects} project/s.");
+
+
+
+
+        }
+    }
+}
